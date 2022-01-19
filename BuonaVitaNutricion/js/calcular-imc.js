4052cd9bd57a017d45dd3ed11fee0ade44971a80
Cholo0022 <andres.bozzani@gmail.com>
2022-01-19 16:36:20 -0300

    Validacion de datos del formulario

--- BuonaVitaNutricion/js/calcular-imc.js
+++ BuonaVitaNutricion/js/calcular-imc.js
@@ -1,49 +1,51 @@
 var pacientes = document.querySelectorAll(".paciente");
 
+for (var i = 0; i < pacientes.length; i++) {
+  var paciente = pacientes[i];
 
-for(var i = 0;i< pacientes.length;i++){
-    var paciente = pacientes[i];
+  var tdPeso = paciente.querySelector(".info-peso");
+  var peso = tdPeso.textContent;
 
-    var tdPeso = paciente.querySelector(".info-peso");
-    var peso = tdPeso.textContent;
+  var tdAltura = paciente.querySelector(".info-altura");
+  var altura = tdAltura.textContent;
 
-    var tdAltura = paciente.querySelector(".info-altura");
-    var altura = tdAltura.textContent;
+  var tdIMC = paciente.querySelector(".info-imc");
 
-    var tdIMC = paciente.querySelector(".info-imc");
+  pesoEsValido = validarPeso(peso);
+  alturaEsValida = validarAltura(altura);
 
+  if (!pesoEsValido) {
+    console.log("Peso incorrecto");
+    tdIMC.textContent = "Peso incorrecto";
+    pesoEsValido = false;
+    paciente.classList.add("paciente-incorrecto");
+  }
 
-    pesoEsValido = true;
-    alturaEsValida = true;
+  if (!alturaEsValida) {
+    console.log("Altura incorrecta");
+    tdIMC.textContent = "Altura incorrecta";
+    alturaEsValida = false;
+    paciente.classList.add("paciente-incorrecto");
+  }
 
-    if((peso < 0) || (peso > 1000)){
-        console.log("Peso incorrecto");
-        tdIMC.textContent = "Peso incorrecto";
-        pesoEsValido = false;
-        paciente.classList.add("paciente-incorrecto");
-    }
-
-    if((altura < 0) || (altura > 3.00)){
-        console.log("Altura incorrecta");
-        tdIMC.textContent = "Altura incorrecta";
-        alturaEsValida = false;
-        paciente.classList.add("paciente-incorrecto");
-    }
-
-    if(pesoEsValido && alturaEsValida){
-        tdIMC.textContent = calcularIMC(peso,altura);
-    }
-
+  if (pesoEsValido && alturaEsValida) {
+    tdIMC.textContent = calcularIMC(peso, altura);
+  }
 }
 
-function calcularIMC(peso,altura){
-    var imc = peso / (altura * altura);
-    return imc.toFixed(2);
+function validarPeso(peso) {
+  if (peso > 0 && peso < 1000) {
+    return true;
+  }
 }
 
+function validarAltura(altura) {
+  if (altura > 0 && altura < 3.0) {
+    return true;
+  }
+}
 
-
-
- 
-
-
+function calcularIMC(peso, altura) {
+  var imc = peso / (altura * altura);
+  return imc.toFixed(2);
+}
--- BuonaVitaNutricion/js/form.js
+++ BuonaVitaNutricion/js/form.js
@@ -4,41 +4,85 @@ botonAdicionar.addEventListener("click", function (event) {
   event.preventDefault();
 
   var form = document.querySelector("#form-adicionar");
+  var paciente = capturarDatosPaciente(form);
+  var pacienteTr = crearPacienteTr(paciente);
+
+  var errores = validarPaciente(paciente);
+  if (errores.length > 0) {
+    mostrarMensajesErrores(errores);
+    return;
+  }
 
   var tabla = document.querySelector("#tabla-pacientes");
-
-  var paciente = capturarDatosPaciente(form);
-  console.log(paciente);
-
-  pacienteTr = document.createElement("tr");
-  nombreTd = document.createElement("td");
-  alturaTd = document.createElement("td");
-  pesoTd = document.createElement("td");
-  gorduraTd = document.createElement("td");
-  imcTd = document.createElement("td");
-
-  nombreTd.textContent = paciente.nombre;
-  alturaTd.textContent = paciente.altura;
-  pesoTd.textContent = paciente.peso;
-  gorduraTd.textContent = paciente.gordura;
-  imcTd.textContent = paciente.imc;
-
-  pacienteTr.appendChild(nombreTd);
-  pacienteTr.appendChild(pesoTd);
-  pacienteTr.appendChild(alturaTd);
-  pacienteTr.appendChild(gorduraTd);
-  pacienteTr.appendChild(imcTd);
-
   tabla.appendChild(pacienteTr);
+  form.reset();
 
-  function capturarDatosPaciente(form) {
-    var paciente = {
-      nombre: form.nombre.value,
-      peso: form.peso.value,
-      altura: form.altura.value,
-      gordura: form.gordura.value,
-      imc: calcularIMC(form.peso.value, form.altura.value),
-    };
-    return paciente;
-  }
+  var mensajesErrores = document.querySelector("#mensajes-errores");
+  mensajesErrores.innerHTML = "";
 });
+
+function capturarDatosPaciente(form) {
+  var paciente = {
+    nombre: form.nombre.value,
+    peso: form.peso.value,
+    altura: form.altura.value,
+    gordura: form.gordura.value,
+    imc: calcularIMC(form.peso.value, form.altura.value),
+  };
+  return paciente;
+}
+
+function crearPacienteTr(paciente) {
+  var pacienteTr = document.createElement("tr");
+  pacienteTr.classList.add("paciente");
+
+  pacienteTr.appendChild(crearTd(paciente.nombre, "info-nombre"));
+  pacienteTr.appendChild(crearTd(paciente.peso, "info-peso"));
+  pacienteTr.appendChild(crearTd(paciente.altura, "info-altura"));
+  pacienteTr.appendChild(crearTd(paciente.gordura, "info-gordura"));
+  pacienteTr.appendChild(crearTd(paciente.imc, "info-imc"));
+
+  return pacienteTr;
+}
+
+function crearTd(dato, clase) {
+  var td = document.createElement("td");
+  td.classList.add(clase);
+  td.textContent = dato;
+  return td;
+}
+
+function validarPaciente(paciente) {
+  var errores = [];
+
+  if (paciente.nombre.length == 0) {
+    errores.push("Debe agregar un nombre al paciente");
+  }
+  if (paciente.peso.length == 0) {
+    errores.push("Debe agregar un peso al paciente");
+  }
+  if (paciente.altura.length == 0) {
+    errores.push("Debe agregar una altura al paciente");
+  }
+  if (paciente.gordura.length == 0) {
+    errores.push("Debe agregar el % Gordura al paciente");
+  }
+  if (!validarPeso(paciente.peso)) {
+    errores.push("El peso del paciente es incorrecto");
+  }
+  if (!validarAltura(paciente.altura)) {
+    errores.push("La altura del paciente es incorrecta");
+  }
+  return errores;
+}
+
+function mostrarMensajesErrores(errores) {
+  var ul = document.querySelector("#mensajes-errores");
+  ul.innerHTML = "";
+
+  errores.forEach(function (error) {
+    var li = document.createElement("li");
+    li.textContent = error;
+    ul.appendChild(li);
+  });
+}
