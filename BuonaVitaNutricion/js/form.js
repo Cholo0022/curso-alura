714985e95644db3f6d6ee2e343c073c0b0193508
Cholo0022 <andres.bozzani@gmail.com>
2022-01-19 11:58:48 -0300

    Se creo el objeto paciente

--- BuonaVitaNutricion/js/form.js
+++ BuonaVitaNutricion/js/form.js
@@ -1,36 +1,44 @@
 var botonAdicionar = document.querySelector("#adicionar-paciente");
 
-botonAdicionar.addEventListener("click",function(event){
-    event.preventDefault();
+botonAdicionar.addEventListener("click", function (event) {
+  event.preventDefault();
 
-    var form = document.querySelector("#form-adicionar");
-    
-    var nombre = form.nombre.value;
-    var peso = form.peso.value;
-    var altura = form.altura.value;
-    var gordura = form.gordura.value
-    
-    var tabla = document.querySelector("#tabla-pacientes");
+  var form = document.querySelector("#form-adicionar");
 
-    pacienteTr = document.createElement("tr");     
-    nombreTd = document.createElement("td");
-    alturaTd = document.createElement("td");
-    pesoTd = document.createElement("td");
-    gorduraTd = document.createElement("td");
-    imcTd = document.createElement("td");
+  var tabla = document.querySelector("#tabla-pacientes");
 
-    nombreTd.textContent = nombre;
-    alturaTd.textContent = altura;
-    pesoTd.textContent = peso;
-    gorduraTd.textContent = gordura;
-    imcTd.textContent = calcularIMC(peso,altura);
+  var paciente = capturarDatosPaciente(form);
+  console.log(paciente);
 
-    pacienteTr.appendChild(nombreTd);
-    pacienteTr.appendChild(pesoTd);
-    pacienteTr.appendChild(alturaTd);
-    pacienteTr.appendChild(gorduraTd);
-    pacienteTr.appendChild(imcTd);
+  pacienteTr = document.createElement("tr");
+  nombreTd = document.createElement("td");
+  alturaTd = document.createElement("td");
+  pesoTd = document.createElement("td");
+  gorduraTd = document.createElement("td");
+  imcTd = document.createElement("td");
 
-    tabla.appendChild(pacienteTr);
+  nombreTd.textContent = paciente.nombre;
+  alturaTd.textContent = paciente.altura;
+  pesoTd.textContent = paciente.peso;
+  gorduraTd.textContent = paciente.gordura;
+  imcTd.textContent = paciente.imc;
 
+  pacienteTr.appendChild(nombreTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+  pacienteTr.appendChild(imcTd);
+
+  tabla.appendChild(pacienteTr);
+
+  function capturarDatosPaciente(form) {
+    var paciente = {
+      nombre: form.nombre.value,
+      peso: form.peso.value,
+      altura: form.altura.value,
+      gordura: form.gordura.value,
+      imc: calcularIMC(form.peso.value, form.altura.value),
+    };
+    return paciente;
+  }
 });
